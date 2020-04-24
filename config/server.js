b34f9909b955152ed9955172984bef2b37218039
Andrew <andrew.rutherford.6@gmail.com>
2020-04-24 10:07:12 +0700

    server returns array of 30 unsplash photos

---
+++ config/server.js
@@ -0,0 +1,6 @@
+module.exports = {
+  APPLICATION_ID:
+    process.env.APPLICATION_ID || 'vzpw--DxUebbgs_XvdHnOHyAl52Q8gZaCp-d_Zc4Jgw',
+  SECRET: process.env.SECRET || 'Q_H5yuaqDJSbYGsWE0l_zIlmC6YfuIApL9pozxxaNBU',
+  CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000',
+};
